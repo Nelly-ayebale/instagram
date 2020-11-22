fa60e1f58c8e3b7b7cb9b628c5ef9995fe4a27cd
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-22 15:45:52 +0300

    Added new template folders for registration and login

--- socials/urls.py
+++ socials/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django_registration.backends.one_step.views import RegistrationView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^accounts/register/',RegistrationView.as_view(success_url='/accounts/login/'),name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
