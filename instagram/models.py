d3ff5a0baaf4be3692ff1d7a01b7abe2af36d97f
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-23 08:35:27 +0300

    Deployment to heroku

--- instagram/models.py
+++ instagram/models.py
@@ -9,7 +9,7 @@ from django.dispatch import receiver
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=60,null=True,blank=True)
+    name = models.CharField(max_length=60,blank=True)
     profile_photo = CloudinaryField('image')
     bio = HTMLField()
 
