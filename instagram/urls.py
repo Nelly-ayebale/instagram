22c1c3b9ff4ac6bbdf60dee78b5407d2d8a30252
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-23 05:16:23 +0300

    Added a url for the search

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 urlpatterns=[
     path('home/',views.home,name='home'),
     path('profile/<username>/',views.profile,name='profile'),
-    path('user/<username>/',views.user,name='user')
+    path('user/<username>/',views.user,name='user'),
+    path('search/', views.search, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
