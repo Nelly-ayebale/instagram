629c1d3e38154713c9a78758384fef15604ca92b
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-11-23 08:32:36 +0300

    Deployment to heroku

--- instagram/models.py
+++ instagram/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
     bio = HTMLField()
 
     def __str__(self):
-        return self.user.username}
+        return self.user.username
 
     def save_profile(self):
         self.user
